829f09dd3f312bf7998dde34c640e13d35e7efd7
Todor Daskalov <todorvdaskalov@gmail.com>
2023-09-14 21:23:27 +0300

    Correction in logic for test "test_form__pickup_time_after_current_time"

--- tests/cart/views/test_form.py
+++ tests/cart/views/test_form.py
@@ -21,11 +21,13 @@ class OrderFormTestCase(TestCase):
         else:
             self.assertTrue(form.is_valid())
 
-    def test_form__pickup_time_after_current_time(self):
+    # This test is accurate only in working hours of the bakery
+    def test_form__pickup_time_before_current_time(self):
         current_time = datetime.now().time()
-        form = OrderForm(data={'pickup_time': f'{9}:{30}'})
-        if current_time >= datetime.strptime('09:30', '%H:%M').time():
-            self.assertFalse(form.is_valid())
-        else:
-            self.assertTrue(form.is_valid())
-
+        form = OrderForm(data={'pickup_time': f'{9}:{00}'})
+        if not (current_time < datetime.strptime('09:00', '%H:%M').time() or
+                current_time > datetime.strptime('19:50', '%H:%M').time()):
+            if current_time >= datetime.strptime('09:00', '%H:%M').time():
+                self.assertFalse(form.is_valid())
+            else:
+                self.assertTrue(form.is_valid())
